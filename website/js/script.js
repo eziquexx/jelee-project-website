dd73bb402db15ab6167db82c1421d498d9ae2eed
jelee <waftyann@gmail.com>
2024-09-04 18:00:20 +0900

    작업

--- website/js/script.js
+++ website/js/script.js
@@ -16,28 +16,28 @@ function cardStyle() {
       
       // windowW >= 1200 경우, 4의 배수 카드는 marginRgiht 없음.
       if (windowW >= 1200) {
-        cards[i].style.width = "24%";
+        cards[i].style.width = "23%";
         cards[i].style.marginRight = "0";
-        cards[i].style.marginBottom = "1.3%";
+        cards[i].style.marginBottom = "4%";
         if (i % 4 === 0 ) {
           continue;
         }
-        cards[i-1].style.marginRight = "1.3%";
+        cards[i-1].style.marginRight = "2.5%";
       } else if (windowW >= 800) {
         // windowW >= 800 경우, 3의 배수 카드는 marginRgiht 없음.
-        cards[i].style.width = "32%";
+        cards[i].style.width = "31%";
         cards[i].style.marginRight = "0";
-        cards[i].style.marginBottom = "2%";
+        cards[i].style.marginBottom = "4%";
         if (i % 3 === 0 ) {
           continue;
         }
         console.log(i-1);
-        cards[i-1].style.marginRight = "2%";
+        cards[i-1].style.marginRight = "3.5%";
       } else if (windowW >= 600) {
         // windowW >= 600 경우, 2의 배수 카드는 marginRgiht 없음.
         cards[i].style.width = "48%";
         cards[i].style.marginRight = "0";
-        cards[i].style.marginBottom = "4%";
+        cards[i].style.marginBottom = "6%";
         if (i % 2 === 0 ) {
           continue;
         }
@@ -45,7 +45,7 @@ function cardStyle() {
       } else {
         // windowW 600 이하 경우, 2의 배수 카드는 marginRgiht 없음.
         cards[i].style.width = "80%";
-        cards[i].style.margin = "0 auto 4%";
+        cards[i].style.margin = "0 auto 12%";
       }
     }
   });
