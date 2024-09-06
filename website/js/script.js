afb41bf103ae23b47de3fa53a941d4817c649378
jelee <waftyann@gmail.com>
2024-09-06 17:59:14 +0900

    navbar 2차 작업중

--- website/js/script.js
+++ website/js/script.js
@@ -2,7 +2,36 @@ const header = document.getElementById("header");
 const headerHeight = header.offsetHeight;
 const cardSection = document.getElementById("cardSection");
 
+const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtnShape = document.querySelector(".toggle-btn-shape");
 
+window.addEventListener("wheel", function(event) {
+  let wheelMove = event.deltaY * -0.01;
+  // wheel down -1, wheel up +1
+  if (wheelMove === 1) {
+    header.style.transform = `translateY(0px)`;
+    if (this.window.scrollY > 1) {
+      header.style.backgroundColor = "#fff";
+      header.style.borderBottom = "1px solid #ddd";
+      header.style.boxShadow = "0px 1px 20px -10px rgba(0, 0, 0, 0.3)";
+      header.style.padding = "25px 0";
+    } else {
+      // header.style.backgroundColor = "transparent";
+      // header.style.borderBottom = "none";
+      // header.style.boxShadow = "none";
+      // header.style.padding = "0";
+    }
+    //scrollY가 1미만이면 배경 안 보임. 1이상이면 배경과 togglebtn배경색 보임.
+  } else if (wheelMove === -1 && this.window.scrollY > 1) {
+    header.style.transform = `translateY(-${parseFloat(headerHeight)}px)`;
+    // header.style.padding = "0";
+  }
+});
+
+// toggleBtn click시 모양 변경
+toggleBtn.addEventListener("click", function() {
+  toggleBtnShape.classList.toggle("on");
+});
 
 
 // header와 cardSection 거리
@@ -32,7 +61,6 @@ function resizeStyle() {
         if (i % 3 === 0 ) {
           continue;
         }
-        console.log(i-1);
         cards[i-1].style.marginRight = "3.5%";
       } else if (windowW >= 600) {
         // windowW >= 600 경우, 2의 배수 카드는 marginRgiht 없음.
