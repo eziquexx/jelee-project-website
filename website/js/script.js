c42683850361459f8e56b812b28e12cde2432ef1
eziquexx <waftyann@gmail.com>
2024-09-07 23:30:10 +0900

    navbar(header) 수정 완료

--- website/js/script.js
+++ website/js/script.js
@@ -1,40 +1,60 @@
 const header = document.getElementById("header");
+const mainMenu = document.getElementById("menu");
 const headerHeight = header.offsetHeight;
 const cardSection = document.getElementById("cardSection");
-
-const toggleBtn = document.querySelector(".toggle-btn");
+const toggleBtn = document.getElementById("toggle-btn");
+const toggleMenu = document.getElementById("toggle-menu");
 const toggleBtnShape = document.querySelector(".toggle-btn-shape");
 
 window.addEventListener("wheel", function(event) {
   let wheelMove = event.deltaY * -0.01;
-  // wheel down -1, wheel up +1
-  if (wheelMove === 1) {
-    header.style.transform = `translateY(0px)`;
-    if (this.window.scrollY > 1) {
+
+  // toggleMenu가 block이 아니면 실행하게 만들기.
+  if (toggleMenu.style.display !== "block") {
+    // wheel down -1, wheel up +1
+    // wheel down하면 navbar 안 보이게, wheel up하면 navbar 보이게
+    if (wheelMove === 1) {
+      header.style.transform = "translateY(0px)";
+    } else if (wheelMove === -1 && this.window.scrollY >= 0) {
+      //scrollY가 1미만이면 배경 안 보임. 1이상이면 배경과 togglebtn배경색 보임.
+      header.style.transform = "translateY(-150px)";
+    }
+    // 스크롤 위치에 따라 navbar(header) 스타일 조정
+    if (this.window.scrollY <= 100) {
+      header.style.backgroundColor = "transparent";
+      header.style.borderBottom = "none";
+      header.style.boxShadow = "none";
+    } else {
       header.style.backgroundColor = "#fff";
       header.style.borderBottom = "1px solid #ddd";
       header.style.boxShadow = "0px 1px 20px -10px rgba(0, 0, 0, 0.3)";
-      header.style.padding = "25px 0";
-    } else {
-      // header.style.backgroundColor = "transparent";
-      // header.style.borderBottom = "none";
-      // header.style.boxShadow = "none";
-      // header.style.padding = "0";
     }
-    //scrollY가 1미만이면 배경 안 보임. 1이상이면 배경과 togglebtn배경색 보임.
-  } else if (wheelMove === -1 && this.window.scrollY > 1) {
-    header.style.transform = `translateY(-${parseFloat(headerHeight)}px)`;
-    // header.style.padding = "0";
   }
+  
 });
 
-// toggleBtn click시 모양 변경
+
+// toggleBtn click 이벤트
 toggleBtn.addEventListener("click", function() {
+
   toggleBtnShape.classList.toggle("on");
+
+  if (toggleMenu.style.display === "block") {
+    toggleMenu.style.display = "none";
+    document.body.style.overflow = 'auto';
+  } else {
+    toggleMenu.style.display = "block";
+    document.body.style.overflow = 'hidden';
+    header.style.backgroundColor = "#fff";
+    header.style.borderBottom = "1px solid #ddd";
+    header.style.boxShadow = "0px 1px 20px -10px rgba(0, 0, 0, 0.3)";
+    header.style.transform = "translateY(0px)";
+  }
+
 });
 
 
-// header와 cardSection 거리
+// header와 cardSection 거리 지정
 cardSection.style.marginTop = headerHeight + 70 + "px";
 
 function resizeStyle() {
@@ -82,3 +102,14 @@ function resizeStyle() {
 
 window.addEventListener("resize", resizeStyle);
 resizeStyle();
+
+
+// 로딩되었을 때 스크롤 위치를 구하는 코드 구하기.
+window.addEventListener("load", function(){
+  const windowY = window.scrollY;
+  if (windowY > 100) {
+    header.style.backgroundColor = "#fff";
+    header.style.borderBottom = "1px solid #ddd";
+    header.style.boxShadow = "0px 1px 20px -10px rgba(0, 0, 0, 0.3)";
+  }
+});
